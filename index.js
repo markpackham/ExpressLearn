87777fd7f681ae9c6917e9306f213d83c85c3f3c
Mark Packham <markpackham1@gmail.com>
2019-02-22 21:22:50 +0000

    https://youtu.be/L72fhGm1tfE?t=3576 template engine handlebars

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
-
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
+const moment = require("moment");
 const app = express();
+
+//Init middleware
+app.unsubscribe(logger);
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +20,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 */
+
+//Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
